a78a14891fb64da5c75b94e5f2374c5c055d7bd3
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2025-06-05 23:31:16 +0100

    file

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import ProfileImage from "../../src/assets/images/bessie.png";
 import Edit from "../../src/assets/images/icons/edit_24dp_1F1F1F.png";
+import EditProfileModal from "./EditProfile";
 
 const Profile = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const handleEditClick = () => {
+    setIsModalOpen(true);
+  };
   return (
     <>
-    
       <div className="profile">
         <div className="profile_user_info">
           <img
@@ -18,7 +22,7 @@ const Profile = () => {
             <h3>Bessie Coleman</h3>
             <span className="title">Civil Aviator</span>
             <div className="edit_profile">
-              <img src={Edit} alt="Edit icon" />
+              <img src={Edit} alt="Edit icon" onClick={handleEditClick} />
               <span>Edit Profile</span>
             </div>
           </div>
@@ -29,6 +33,9 @@ const Profile = () => {
         </button>
       </div>
       <hr />
+      {isModalOpen && (
+        <EditProfileModal onClose={() => setIsModalOpen(false)} />
+      )}
     </>
   );
 };
